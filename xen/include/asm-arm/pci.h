ac07fda78da591f85d04c514bad6b158089440e4
Manish Jaggi <mjaggi@caviumnetworks.com>
2018-03-07 13:47:51 +0530

    Fix the #include label in asm-arm/pci.h

--- xen/include/asm-arm/pci.h
+++ xen/include/asm-arm/pci.h
@@ -1,7 +1,7 @@
-#ifndef __X86_PCI_H__
-#define __X86_PCI_H__
+#ifndef __ASM_ARM_PCI_H__
+#define __ASM_ARM_PCI_H__
 
 struct arch_pci_dev {
 };
 
-#endif /* __X86_PCI_H__ */
+#endif /* __ASM_ARM_PCI_H__ */
